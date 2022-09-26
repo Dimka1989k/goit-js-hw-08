8d016d82ed6fcc501e05ffc98f632529509c6935
Dimon1989k <dima1989k@ukr.net>
2022-09-26 15:39:45 +0300

    add library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+
+const gallery = document.querySelector('.gallery');
+
+const createGalleryTemplate = ({ preview, original, description }) => `
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+`;
+
+const getImagesGallery = images =>
+  images.map(item => createGalleryTemplate(item)).join('');
+
+gallery.innerHTML = getImagesGallery(galleryItems);
+gallery.addEventListener('click', handleClick);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'top',
+});
+
+function handleClick(e) {
+  e.preventDefault();
+}
 console.log(galleryItems);
