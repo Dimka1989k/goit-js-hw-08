bd5a1c9ff806b04d54d27b1f6c8d6c78dfeda7d1
Dimon1989k <dima1989k@ukr.net>
2022-09-27 15:57:51 +0300

    finish 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'selectedFilters';
+const formRef = document.querySelector('.feedback-form');
+initForm();
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(handleSubmit) {
+  handleSubmit.preventDefault();
+  const formData = new FormData(formRef);
+  formData.forEach((value, name) => console.log(value, name));
+  handleSubmit.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function onFormInput(handleSubmit) {
+  let persistedFilters = localStorage.getItem(LOCALSTORAGE_KEY);
+  persistedFilters = persistedFilters ? JSON.parse(persistedFilters) : {};
+  persistedFilters[handleSubmit.target.name] = handleSubmit.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(persistedFilters));
+}
+
+function initForm() {
+  let persistedFilters = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (persistedFilters) {
+    persistedFilters = JSON.parse(persistedFilters);
+    Object.entries(persistedFilters).forEach(([name, value]) => {
+      formRef.elements[name].value = value;
+    });
+  }
+}
